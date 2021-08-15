01016cec0d115e16cdd56891dba586201827e57b
norrkin <norrkin@icloud.com>
2021-08-15 13:48:48 +0100

    fixed formatting

--- main.tf
+++ main.tf
@@ -4,11 +4,11 @@ terraform {
       version = "=2.70.0"
     }
   }
-backend "azurerm" {
-  resource_group_name  = "tfstate"
-  storage_account_name = "tfstate97921246"
-  container_name       = "tfstate"
-  key                  = "terraform-azurerm-backend.tfstate"
+  backend "azurerm" {
+    resource_group_name  = "tfstate"
+    storage_account_name = "tfstate97921246"
+    container_name       = "tfstate"
+    key                  = "terraform-azurerm-backend.tfstate"
   }
 }
 
