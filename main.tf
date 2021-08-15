571e4e81406f5c84300ede804cae724b3c60b1b0
norrkin <norrkin@icloud.com>
2021-08-15 11:06:28 +0100

    enabled actions

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    azurerm = {
+      version = "=2.70.0"
+    }
+  }
+backend "azurerm" {
+  resource_group_name  = "tfstate"
+  storage_account_name = "tfstate97921246"
+  container_name       = "tfstate"
+  key                  = "terraform-azurerm-backend.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "terraform-azurerm-backend" {
+  name     = "terraform-azurerm-backend"
+  location = "uksouth"
+}
