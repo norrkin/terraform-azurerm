72f530e8ea9810a077342f7e18436c65f4dfa905
norrkin <norrkin@icloud.com>
2021-08-16 19:36:43 +0100

    added vnet

--- main.tf
+++ main.tf
@@ -17,6 +17,13 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "terraform-azurerm-backend" {
-  name     = "terraform-azurerm-backend"
-  location = "uksouth"
+  name     = var.resource_group_name
+  location = var.resource_group_location
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "terraform-azurerm-backend-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = var.resource_group_location
+  resource_group_name = var.resource_group_name
 }
--- variables.tf
+++ variables.tf
@@ -1,10 +1,9 @@
 variable "resource_group_name" {
-  type        = string
-  description = "Name of the resource group where resources are to be deployed"
+  type    = string
+  default = "terraform-azurerm-backend"
 }
 
 variable "resource_group_location" {
-  type        = string
-  description = "Location of the resource group where resources are to be deployed"
-  default     = "uksouth"
+  type    = string
+  default = "uksouth"
 }
