eb9140f05c3cbe6306a17256bbc1c526a86456f9
norrkin <norrkin@icloud.com>
2021-08-17 11:42:17 +0100

    split provider/backend

--- main.tf
+++ main.tf
@@ -1,21 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      version = "=2.70.0"
-    }
-  }
-  backend "azurerm" {
-    resource_group_name  = "tfstate"
-    storage_account_name = "tfstate97921246"
-    container_name       = "tfstate"
-    key                  = "terraform-azurerm-backend.tfstate"
-  }
-}
-
-provider "azurerm" {
-  features {}
-}
-
 resource "azurerm_resource_group" "terraform-azurerm-backend" {
   name     = var.resource_group_name
   location = var.resource_group_location
