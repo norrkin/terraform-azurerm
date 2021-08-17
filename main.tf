2ad1bc56deaaea95dd8d1a12e98a586bc6bbc223
norrkin <norrkin@icloud.com>
2021-08-17 16:05:51 +0100

    oops, I broke it

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "azurerm_resource_group" "rg" {
+resource "azurerm_resource_group" "terraform-azurerm-backend" {
   name     = var.resource_group_name
   location = var.location
 
@@ -8,23 +8,23 @@ resource "azurerm_resource_group" "rg" {
 resource "azurerm_virtual_network" "vnet" {
   name                = "terraform-azurerm-backend-vnet"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.terraform-azurerm-backend.location
+  resource_group_name = azurerm_resource_group.terraform-azurerm-backend.name
 
   tags = local.tags
 }
 
 resource "azurerm_subnet" "subnet" {
   name                 = "terraform-azurerm-backend-subnet"
-  resource_group_name  = azurerm_resource_group.rg.name
+  resource_group_name  = azurerm_resource_group.terraform-azurerm-backend.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.8.0/24"]
 }
 
 resource "azurerm_network_security_group" "security_group" {
   name                = "security_group"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.terraform-azurerm-backend.location
+  resource_group_name = azurerm_resource_group.terraform-azurerm-backend.name
 
   security_rule {
     name                       = "test123"
